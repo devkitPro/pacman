d28415a3b76c51253c37d192e68e630fe034bf5a
Aurelien Foret <aurelien@archlinux.org>
2006-01-01 10:18:22 +0000

    renamed error titles from XXX to xxx

--- src/pacman/log.c
+++ src/pacman/log.c
@@ -45,22 +45,22 @@ void cb_log(unsigned short level, char *msg)
 
 	switch(level) {
 		case PM_LOG_DEBUG:
-			sprintf(str, "DEBUG");
+			sprintf(str, "debug");
 		break;
 		case PM_LOG_ERROR:
-			sprintf(str, "ERROR");
+			sprintf(str, "error");
 		break;
 		case PM_LOG_WARNING:
-			sprintf(str, "WARNING");
+			sprintf(str, "warning");
 		break;
 		case PM_LOG_FLOW1:
-			sprintf(str, "FLOW1");
+			sprintf(str, "flow1");
 		break;
 		case PM_LOG_FLOW2:
-			sprintf(str, "FLOW2");
+			sprintf(str, "flow2");
 		break;
 		case PM_LOG_FUNCTION:
-			sprintf(str, "FUNCTION");
+			sprintf(str, "function");
 		break;
 		default:
 			sprintf(str, "???");
