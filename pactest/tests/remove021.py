352913b9941441c9eb60471e07080e74ebc4eb71
Dan McGee <dan@archlinux.org>
2007-02-23 07:04:12 +0000

    * Oops. Messed a few things up, fixing them now.

--- pactest/tests/remove021.py
+++ pactest/tests/remove021.py
@@ -5,7 +5,7 @@ p1.files = ["etc/dummy.conf*"]
 p1.backup = ["etc/dummy.conf"]
 self.addpkg2db("local", p1)
 
-self.args = "-R %s" % p1.name
+self.args = "-Rn %s" % p1.name
 
 self.addrule("PACMAN_RETCODE=0")
 self.addrule("!PKG_EXIST=dummy")
