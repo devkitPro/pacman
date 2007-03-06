294eff60219bb023b9934026331b162bfbb7dc0c
Dan McGee <dan@archlinux.org>
2007-03-06 06:38:20 +0000

    Trying to fix up this autotools stuff a bit more.
    * Greatly simplify and de-obfuscate the autogen and autoclean code.
    * Add a bunch of the autotools required files to the repository. This
      will give us consistency (a novel idea) across all builds, and allow
      for much more recent versions of config.guess and config.sub to be used.

--- autogen.sh
+++ autogen.sh
@@ -1,51 +1,6 @@
-#!/bin/sh -e
+#!/bin/sh -xu
 
-if [ "$1" == "--gettext-only" ]; then
-	sh autoclean.sh
-	for i in lib/libalpm/po src/pacman/po
-	do
-		cd $i
-		mv Makevars Makevars.tmp
-		package=`pwd|sed 's|.*/\(.*\)/.*|\1|'`
-		intltool-update --pot --gettext-package=$package
-		for j in *.po
-		do
-			if msgmerge $j $package.pot -o $j.new; then
-				mv -f $j.new $j
-				echo -n "$i/$j: "
-				msgfmt -c --statistics -o $j.gmo $j
-				rm -f $j.gmo
-			else
-				echo "msgmerge for $j failed!"
-				rm -f $j.new
-			fi
-		done
-		mv Makevars.tmp Makevars
-		cd - >/dev/null
-	done
-	cd doc
-	po4a -k 0 po4a.cfg
-	cd po
-	for i in *po
-	do
-		if msgmerge $i $package.pot -o $i.new; then
-			mv -f $i.new $i
-			echo -n "man/$i: "
-			msgfmt -c --statistics -o $i.gmo $i
-			rm -f $i.gmo
-		else
-			echo "msgmerge for $i failed!"
-			rm -f $i.new
-		fi
-	done
-	exit 0
-fi
-
-cp -f $(dirname $(which automake))/../share/automake-*/mkinstalldirs ./
-cp -f $(dirname $(which automake))/../share/gettext/config.rpath ./
-
-libtoolize -f -c
-aclocal --force
-autoheader -f
-autoconf -f
-automake -a -c --gnu --foreign
+aclocal
+autoheader
+automake --foreign
+autoconf
