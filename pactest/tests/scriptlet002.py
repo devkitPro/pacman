1c2358532fac7588927f5bdf122493e4cc4fb9f8
Dan McGee <dan@archlinux.org>
2007-04-07 16:27:32 -0400

    Add pactest for pre_ and post_remove scriptlet events firing
    Signed-off-by: Dan McGee <dan@archlinux.org>

---
+++ pactest/tests/scriptlet002.py
@@ -0,0 +1,16 @@
+# quick note here - chroot() is expected to fail.  We're not checking the
+# validity of the scripts, only that they fire (or try to)
+self.description = "Scriptlet test (pre/post remove)"
+
+p1 = pmpkg("dummy")
+p1.files = ['etc/dummy.conf']
+p1.install['pre_remove'] = "ls /etc";
+p1.install['post_remove'] = "ls /etc";
+self.addpkg2db("local", p1)
+
+# --debug is necessary to check PACMAN_OUTPUT
+self.args = "--debug -R %s" % p1.name
+
+self.addrule("PACMAN_RETCODE=0")
+self.addrule("PACMAN_OUTPUT=pre_remove")
+self.addrule("PACMAN_OUTPUT=post_remove")
