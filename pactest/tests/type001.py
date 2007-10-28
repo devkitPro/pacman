0dc34f496dca3d916eae85a0357049b8b9d711bb
Chantry Xavier <shiningxc@gmail.com>
2007-10-28 13:37:49 +0100

    extend type001 pactest with a symlink.
    Signed-off-by: Chantry Xavier <shiningxc@gmail.com>

--- pactest/tests/type001.py
+++ pactest/tests/type001.py
@@ -1,13 +1,17 @@
 self.description = "Check the types of default files in a package"
 
 p = pmpkg("pkg1")
-p.files = ["bin/foo"
-           "bin/bar"]
+p.files = ["foo/file1",
+           "foo/file2",
+           "foo/dir/",
+           "foo/symdir -> dir"]
 self.addpkg(p)
 
 self.args = "-U %s" % p.filename()
 
 self.addrule("PACMAN_RETCODE=0")
-for f in p.files:
-	self.addrule("FILE_TYPE=%s|file" % f)
-self.addrule("FILE_TYPE=bin/|dir")
+self.addrule("FILE_TYPE=foo/|dir")
+self.addrule("FILE_TYPE=foo/file1|file")
+self.addrule("FILE_TYPE=foo/file2|file")
+self.addrule("FILE_TYPE=foo/dir|dir")
+self.addrule("FILE_TYPE=foo/symdir|link")
