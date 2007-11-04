devkitPro/pacman

commit ad691001e20272b794d2ed574b556f520e3555c0

Remove scriptlet START and DONE commands that we don't use
The scriptlet calling had some unneeded complexity for the time being
which we aren't using here. Let's get rid of it until we find a good way
to implement it correctly.

Signed-off-by: Dan McGee <dan@archlinux.org>

lib/libalpm/alpm.h
@@ -287,9 +287,6 @@ typedef enum _pmtransevt_t {
 	PM_TRANS_EVT_EXTRACT_DONE,
 	PM_TRANS_EVT_INTEGRITY_START,
 	PM_TRANS_EVT_INTEGRITY_DONE,
-	PM_TRANS_EVT_SCRIPTLET_INFO,
-	PM_TRANS_EVT_SCRIPTLET_START,
-	PM_TRANS_EVT_SCRIPTLET_DONE,
 	PM_TRANS_EVT_PRINTURI,
 	PM_TRANS_EVT_RETRIEVE_START,
 } pmtransevt_t;

lib/libalpm/trans.c
@@ -642,7 +642,6 @@ int _alpm_runscriptlet(const char *root, const char *installfn,
 	}
 
 	if(pid == 0) {
-		FILE *pp;
 		_alpm_log(PM_LOG_DEBUG, _("chrooting in %s"), root);
 		if(chroot(root) != 0) {
 			_alpm_log(PM_LOG_ERROR, _("could not change the root directory (%s)"), strerror(errno));
@@ -654,37 +653,7 @@ int _alpm_runscriptlet(const char *root, const char *installfn,
 		}
 		umask(0022);
 		_alpm_log(PM_LOG_DEBUG, _("executing \"%s\""), cmdline);
-		pp = popen(cmdline, "r");
-		if(!pp) {
-			_alpm_log(PM_LOG_ERROR, _("call to popen failed (%s)"), strerror(errno));
-			retval = 1;
-			goto cleanup;
-		}
-		while(!feof(pp)) {
-			char line[1024];
-			if(fgets(line, 1024, pp) == NULL)
-				break;
-			/*TODO clean this code up, remove weird SCRIPTLET_START/DONE,
-			 * (void*)atol call, etc. */
-			/* "START <event desc>" */
-			if((strlen(line) > strlen(SCRIPTLET_START))
-			   && !strncmp(line, SCRIPTLET_START, strlen(SCRIPTLET_START))) {
-				EVENT(trans, PM_TRANS_EVT_SCRIPTLET_START,
-				      _alpm_strtrim(line + strlen(SCRIPTLET_START)), NULL);
-			/* "DONE <ret code>" */
-			} else if((strlen(line) > strlen(SCRIPTLET_DONE))
-			          && !strncmp(line, SCRIPTLET_DONE, strlen(SCRIPTLET_DONE))) {
-				EVENT(trans, PM_TRANS_EVT_SCRIPTLET_DONE,
-				      (void*)atol(_alpm_strtrim(line + strlen(SCRIPTLET_DONE))),
-				      NULL);
-			} else {
-				_alpm_strtrim(line);
-				/* log our script output */
-				alpm_logaction(line);
-				EVENT(trans, PM_TRANS_EVT_SCRIPTLET_INFO, line, NULL);
-			}
-		}
-		pclose(pp);
+		execl("/bin/sh", "sh", "-c", cmdline, (char *)NULL);
 		exit(0);
 	} else {
 		if(waitpid(pid, 0, 0) == -1) {

src/pacman/callback.c
@@ -237,20 +237,6 @@ void cb_trans_evt(pmtransevt_t event, void *data1, void *data2)
 		case PM_TRANS_EVT_INTEGRITY_DONE:
 			printf(_("done.\n"));
 			break;
-		case PM_TRANS_EVT_SCRIPTLET_INFO:
-			printf("%s\n", (char*)data1);
-			break;
-		case PM_TRANS_EVT_SCRIPTLET_START:
-			printf((char*)data1);
-			printf("...");
-			break;
-		case PM_TRANS_EVT_SCRIPTLET_DONE:
-			if(!(long)data1) {
-				printf(_("done.\n"));
-			} else {
-				printf(_("failed.\n"));
-			}
-			break;
 		case PM_TRANS_EVT_PRINTURI:
 			printf("%s/%s\n", (char*)data1, (char*)data2);
 			break;

lib/libalpm/util.h
@@ -40,10 +40,6 @@
 #define _(s) s
 #endif
 
-/*TODO wtf? why is this done like this? */
-#define SCRIPTLET_START "START "
-#define SCRIPTLET_DONE "DONE "
-
 int _alpm_makepath(const char *path);
 int _alpm_copyfile(const char *src, const char *dest);
 char *_alpm_strtoupper(char *str);

commit 006387828cbdd11e6307879ad27e9bb9409ca193

Readd scriptlet logging that got lost in an earlier commit
I broke scriptlet logging with ad691001e20272b794d2ed574b556f520e3555c0.
Readd more or less what was there before, although it still needs a lot of
work including hopefully rewriting it to a new event subsystem and having
it log to a seperate file.

Signed-off-by: Dan McGee <dan@archlinux.org>

lib/libalpm/add.c
@@ -696,7 +696,7 @@ static int commit_single_pkg(pmpkg_t *newpkg, int pkg_current, int pkg_count,
 		/* pre_upgrade scriptlet */
 		if(alpm_pkg_has_scriptlet(newpkg) && !(trans->flags & PM_TRANS_FLAG_NOSCRIPTLET)) {
 			_alpm_runscriptlet(handle->root, newpkg->origin_data.file,
-					"pre_upgrade", newpkg->version, oldpkg->version);
+					"pre_upgrade", newpkg->version, oldpkg->version, trans);
 		}
 	} else {
 		is_upgrade = 0;
@@ -708,7 +708,7 @@ static int commit_single_pkg(pmpkg_t *newpkg, int pkg_current, int pkg_count,
 		/* pre_install scriptlet */
 		if(alpm_pkg_has_scriptlet(newpkg) && !(trans->flags & PM_TRANS_FLAG_NOSCRIPTLET)) {
 			_alpm_runscriptlet(handle->root, newpkg->origin_data.file,
-					"pre_install", newpkg->version, NULL);
+					"pre_install", newpkg->version, NULL, trans);
 		}
 	}
 
@@ -845,10 +845,10 @@ static int commit_single_pkg(pmpkg_t *newpkg, int pkg_current, int pkg_count,
 		if(is_upgrade) {
 			_alpm_runscriptlet(handle->root, scriptlet, "post_upgrade",
 					alpm_pkg_get_version(newpkg),
-					oldpkg ? alpm_pkg_get_version(oldpkg) : NULL);
+					oldpkg ? alpm_pkg_get_version(oldpkg) : NULL, trans);
 		} else {
 			_alpm_runscriptlet(handle->root, scriptlet, "post_install",
-					alpm_pkg_get_version(newpkg), NULL);
+					alpm_pkg_get_version(newpkg), NULL, trans);
 		}
 	}
 

lib/libalpm/alpm.h
@@ -304,6 +304,7 @@ typedef enum _pmtransevt_t {
 	PM_TRANS_EVT_DELTA_PATCH_START,
 	PM_TRANS_EVT_DELTA_PATCH_DONE,
 	PM_TRANS_EVT_DELTA_PATCH_FAILED,
+	PM_TRANS_EVT_SCRIPTLET_INFO,
 	PM_TRANS_EVT_PRINTURI,
 	PM_TRANS_EVT_RETRIEVE_START,
 } pmtransevt_t;

lib/libalpm/remove.c
@@ -284,7 +284,7 @@ int _alpm_remove_commit(pmtrans_t *trans, pmdb_t *db)
 			/* run the pre-remove scriptlet if it exists  */
 			if(alpm_pkg_has_scriptlet(info) && !(trans->flags & PM_TRANS_FLAG_NOSCRIPTLET)) {
 				_alpm_runscriptlet(handle->root, scriptlet, "pre_remove",
-				                   alpm_pkg_get_version(info), NULL);
+				                   alpm_pkg_get_version(info), NULL, trans);
 			}
 		}
 
@@ -324,7 +324,7 @@ int _alpm_remove_commit(pmtrans_t *trans, pmdb_t *db)
 			/* run the post-remove script if it exists  */
 			if(alpm_pkg_has_scriptlet(info) && !(trans->flags & PM_TRANS_FLAG_NOSCRIPTLET)) {
 				_alpm_runscriptlet(handle->root, scriptlet, "post_remove",
-													 alpm_pkg_get_version(info), NULL);
+													 alpm_pkg_get_version(info), NULL, trans);
 			}
 		}
 

lib/libalpm/trans.c
@@ -530,7 +530,7 @@ static int grep(const char *fn, const char *needle)
 
 int _alpm_runscriptlet(const char *root, const char *installfn,
 											 const char *script, const char *ver,
-											 const char *oldver)
+											 const char *oldver, pmtrans_t *trans)
 {
 	char scriptfn[PATH_MAX];
 	char cmdline[PATH_MAX];
@@ -624,6 +624,7 @@ int _alpm_runscriptlet(const char *root, const char *installfn,
 	}
 
 	if(pid == 0) {
+		FILE *pipe;
 		/* this code runs for the child only (the actual chroot/exec) */
 		_alpm_log(PM_LOG_DEBUG, "chrooting in %s\n", root);
 		if(chroot(root) != 0) {
@@ -638,7 +639,21 @@ int _alpm_runscriptlet(const char *root, const char *installfn,
 		}
 		umask(0022);
 		_alpm_log(PM_LOG_DEBUG, "executing \"%s\"\n", cmdline);
-		execl("/bin/sh", "sh", "-c", cmdline, (char *)NULL);
+		/* execl("/bin/sh", "sh", "-c", cmdline, (char *)NULL); */
+		pipe = popen(cmdline, "r");
+		if(!pipe) {
+			_alpm_log(PM_LOG_ERROR, _("call to popen failed (%s)"),
+					strerror(errno));
+			retval = 1;
+			goto cleanup;
+		}
+		while(!feof(pipe)) {
+			char line[PATH_MAX];
+			if(fgets(line, PATH_MAX, pipe) == NULL)
+				break;
+			alpm_logaction("%s", line);
+			EVENT(trans, PM_TRANS_EVT_SCRIPTLET_INFO, line, NULL);
+		}
 		exit(0);
 	} else {
 		/* this code runs for the parent only (wait on the child) */

lib/libalpm/trans.h
@@ -81,7 +81,7 @@ int _alpm_trans_commit(pmtrans_t *trans, alpm_list_t **data);
 int _alpm_trans_update_depends(pmtrans_t *trans, pmpkg_t *pkg);
 int _alpm_runscriptlet(const char *root, const char *installfn,
                        const char *script, const char *ver,
-                       const char *oldver);
+                       const char *oldver, pmtrans_t *trans);
 
 #endif /* _ALPM_TRANS_H */
 

src/pacman/callback.c
@@ -256,6 +256,9 @@ void cb_trans_evt(pmtransevt_t event, void *data1, void *data2)
 		case PM_TRANS_EVT_DELTA_PATCH_FAILED:
 			printf(_("failed.\n"));
 			break;
+		case PM_TRANS_EVT_SCRIPTLET_INFO:
+			printf("%s", (char*)data1);
+			break;
 		case PM_TRANS_EVT_PRINTURI:
 			printf("%s/%s\n", (char*)data1, (char*)data2);
 			break;