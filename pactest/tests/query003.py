0bd6fb3bc28b395d5af099221e2c0401d3be42e2
Dan McGee <dan@archlinux.org>
2008-03-30 16:47:27 -0500

    Update -Ss and -Qs pactests to include groups
    This update causes query003 to fail before commit
    4b8ada818e21aae43b4f54f146ffa215fdb9213e. Hopefully the issue doesn't crop
    up again but we might as well test for it.
    
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- pactest/tests/query003.py
+++ pactest/tests/query003.py
@@ -2,6 +2,7 @@ self.description = "Query search for a package"
 
 p = pmpkg("foobar")
 p.files = ["bin/foobar"]
+p.groups = ["group1", "group2"]
 self.addpkg2db("local", p)
 
 self.args = "-Qs %s" % p.name
--- pactest/tests/sync1101.py
+++ pactest/tests/sync1101.py
@@ -1,6 +1,7 @@
 self.description = "Search for package from a sync db"
 
 sp = pmpkg("dummy")
+sp.groups = ["group1", "group2"]
 sp.files = ["bin/dummy",
             "usr/man/man1/dummy.1"]
 self.addpkg2db("sync", sp)
