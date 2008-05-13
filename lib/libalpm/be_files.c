devkitPro/pacman

commit 046003844739416ff6d168dd2dec76490adb0727

Remove some useless abstraction and start db cleanup
We have some useless abstractions like an alpm_db_rewind function. I've read
somewhere that readdir() was the worst filesystem function call invented,
and what do we do? Add a wrapper around it. Kill this abstraction and move
some other things into be_files that should be there anyway because they
are so tied to how a files backend works.

Signed-off-by: Dan McGee <dan@archlinux.org>

lib/libalpm/be_files.c
@@ -36,6 +36,7 @@
 /* libalpm */
 #include "db.h"
 #include "alpm_list.h"
+#include "cache.h"
 #include "log.h"
 #include "util.h"
 #include "alpm.h"
@@ -43,24 +44,163 @@
 #include "package.h"
 #include "delta.h"
 #include "deps.h"
+#include "dload.h"
 
 
-/* This function is used to convert the downloaded db file to the proper backend
- * format
+/*
+ * Return the last update time as number of seconds from the epoch.
+ * Returns 0 if the value is unknown or can't be read.
  */
-int _alpm_db_install(pmdb_t *db, const char *dbfile)
+time_t getlastupdate(const pmdb_t *db)
 {
+	FILE *fp;
+	char *file;
+	time_t ret = 0;
+
 	ALPM_LOG_FUNC;
 
-	/* TODO we should not simply unpack the archive, but better parse it and
-	 * db_write each entry (see sync_load_dbarchive to get archive content) */
-	_alpm_log(PM_LOG_DEBUG, "unpacking database '%s'\n", dbfile);
-
-	if(_alpm_unpack(dbfile, db->path, NULL)) {
-		RET_ERR(PM_ERR_SYSTEM, -1);
+	if(db == NULL) {
+		return(ret);
 	}
 
-	return unlink(dbfile);
+	/* db->path + '.lastupdate' + NULL */
+	MALLOC(file, strlen(db->path) + 12, RET_ERR(PM_ERR_MEMORY, ret));
+	sprintf(file, "%s.lastupdate", db->path);
+
+	/* get the last update time, if it's there */
+	if((fp = fopen(file, "r")) == NULL) {
+		free(file);
+		return(ret);
+	} else {
+		char line[64];
+		if(fgets(line, sizeof(line), fp)) {
+			ret = atol(line);
+		}
+	}
+	fclose(fp);
+	free(file);
+	return(ret);
+}
+
+/*
+ * writes the dbpath/.lastupdate file with the value in time
+ */
+int setlastupdate(const pmdb_t *db, time_t time)
+{
+	FILE *fp;
+	char *file;
+	int ret = 0;
+
+	ALPM_LOG_FUNC;
+
+	if(db == NULL || time == 0) {
+		return(-1);
+	}
+
+	/* db->path + '.lastupdate' + NULL */
+	MALLOC(file, strlen(db->path) + 12, RET_ERR(PM_ERR_MEMORY, ret));
+	sprintf(file, "%s.lastupdate", db->path);
+
+	if((fp = fopen(file, "w")) == NULL) {
+		free(file);
+		return(-1);
+	}
+	if(fprintf(fp, "%ju", (uintmax_t)time) <= 0) {
+		ret = -1;
+	}
+	fclose(fp);
+	free(file);
+	return(ret);
+}
+
+/** Update a package database
+ * @param force if true, then forces the update, otherwise update only in case
+ * the database isn't up to date
+ * @param db pointer to the package database to update
+ * @return 0 on success, > 0 on error (pm_errno is set accordingly), < 0 if up
+ * to date
+ */
+int SYMEXPORT alpm_db_update(int force, pmdb_t *db)
+{
+	alpm_list_t *lp;
+	char path[PATH_MAX];
+	time_t newmtime = 0, lastupdate = 0;
+	const char *dbpath;
+	int ret;
+
+	ALPM_LOG_FUNC;
+
+	/* Sanity checks */
+	ASSERT(handle != NULL, RET_ERR(PM_ERR_HANDLE_NULL, -1));
+	ASSERT(db != NULL && db != handle->db_local, RET_ERR(PM_ERR_WRONG_ARGS, -1));
+	/* Verify we are in a transaction.  This is done _mainly_ because we need a DB
+	 * lock - if we update without a db lock, we may kludge some other pacman
+	 * process that _has_ a lock.
+	 */
+	ASSERT(handle->trans != NULL, RET_ERR(PM_ERR_TRANS_NULL, -1));
+	ASSERT(handle->trans->state == STATE_INITIALIZED, RET_ERR(PM_ERR_TRANS_NOT_INITIALIZED, -1));
+	ASSERT(handle->trans->type == PM_TRANS_TYPE_SYNC, RET_ERR(PM_ERR_TRANS_TYPE, -1));
+
+	if(!alpm_list_find_ptr(handle->dbs_sync, db)) {
+		RET_ERR(PM_ERR_DB_NOT_FOUND, -1);
+	}
+
+	if(!force) {
+		/* get the lastupdate time */
+		lastupdate = getlastupdate(db);
+		if(lastupdate == 0) {
+			_alpm_log(PM_LOG_DEBUG, "failed to get lastupdate time for %s\n",
+					db->treename);
+		}
+	}
+
+	snprintf(path, PATH_MAX, "%s" DBEXT, db->treename);
+	dbpath = alpm_option_get_dbpath();
+
+	ret = _alpm_download_single_file(path, db->servers, dbpath,
+			lastupdate, &newmtime);
+
+	if(ret == 1) {
+		/* mtimes match, do nothing */
+		pm_errno = 0;
+		return(1);
+	} else if(ret == -1) {
+		/* pm_errno was set by the download code */
+		_alpm_log(PM_LOG_DEBUG, "failed to sync db: %s\n", alpm_strerrorlast());
+		return(-1);
+	} else {
+		/* form the path to the db location */
+		snprintf(path, PATH_MAX, "%s%s" DBEXT, dbpath, db->treename);
+
+		/* remove the old dir */
+		_alpm_log(PM_LOG_DEBUG, "flushing database %s\n", db->path);
+		for(lp = _alpm_db_get_pkgcache(db); lp; lp = lp->next) {
+			pmpkg_t *pkg = lp->data;
+			if(pkg && _alpm_db_remove(db, pkg) == -1) {
+				_alpm_log(PM_LOG_ERROR, _("could not remove database entry %s%s\n"), db->treename,
+									alpm_pkg_get_name(pkg));
+				RET_ERR(PM_ERR_DB_REMOVE, -1);
+			}
+		}
+
+		/* Cache needs to be rebuilt */
+		_alpm_db_free_pkgcache(db);
+
+		/* uncompress the sync database */
+		if(_alpm_unpack(path, db->path, NULL)) {
+			RET_ERR(PM_ERR_SYSTEM, -1);
+		}
+		unlink(path);
+
+		/* if we have a new mtime, set the DB last update value */
+		if(newmtime) {
+			_alpm_log(PM_LOG_DEBUG, "sync: new mtime for %s: %ju\n",
+					db->treename, (uintmax_t)newmtime);
+			setlastupdate(db, newmtime);
+		}
+	}
+
+	return(0);
 }
 
 int _alpm_db_open(pmdb_t *db)
@@ -92,17 +232,6 @@ void _alpm_db_close(pmdb_t *db)
 		closedir(db->handle);
 		db->handle = NULL;
 	}
-}
-
-void _alpm_db_rewind(pmdb_t *db)
-{
-	ALPM_LOG_FUNC;
-
-	if(db == NULL || db->handle == NULL) {
-		return;
-	}
-
-	rewinddir(db->handle);
 }
 
 static int splitname(const char *target, pmpkg_t *pkg)
@@ -738,72 +867,6 @@ int _alpm_db_remove(pmdb_t *db, pmpkg_t *info)
 	}
 
 	return(0);
-}
-
-/*
- * Return the last update time as number of seconds from the epoch.
- * Returns 0 if the value is unknown or can't be read.
- */
-time_t _alpm_db_getlastupdate(const pmdb_t *db)
-{
-	FILE *fp;
-	char *file;
-	time_t ret = 0;
-
-	ALPM_LOG_FUNC;
-
-	if(db == NULL) {
-		return(ret);
-	}
-
-	/* db->path + '.lastupdate' + NULL */
-	MALLOC(file, strlen(db->path) + 12, RET_ERR(PM_ERR_MEMORY, ret));
-	sprintf(file, "%s.lastupdate", db->path);
-
-	/* get the last update time, if it's there */
-	if((fp = fopen(file, "r")) == NULL) {
-		free(file);
-		return(ret);
-	} else {
-		char line[64];
-		if(fgets(line, sizeof(line), fp)) {
-			ret = atol(line);
-		}
-	}
-	fclose(fp);
-	free(file);
-	return(ret);
-}
-
-/*
- * writes the dbpath/.lastupdate file with the value in time
- */
-int _alpm_db_setlastupdate(const pmdb_t *db, time_t time)
-{
-	FILE *fp;
-	char *file;
-	int ret = 0;
-
-	ALPM_LOG_FUNC;
-
-	if(db == NULL || time == 0) {
-		return(-1);
-	}
-
-	/* db->path + '.lastupdate' + NULL */
-	MALLOC(file, strlen(db->path) + 12, RET_ERR(PM_ERR_MEMORY, ret));
-	sprintf(file, "%s.lastupdate", db->path);
-
-	if((fp = fopen(file, "w")) == NULL) {
-		free(file);
-		return(-1);
-	}
-	if(fprintf(fp, "%ju", (uintmax_t)time) <= 0) {
-		ret = -1;
-	}
-	fclose(fp);
-	free(file);
-	return(ret);
 }
 
 /* vim: set ts=2 sw=2 noet: */

lib/libalpm/cache.c
@@ -54,7 +54,6 @@ int _alpm_db_load_pkgcache(pmdb_t *db)
 	_alpm_log(PM_LOG_DEBUG, "loading package cache for repository '%s'\n",
 	          db->treename);
 
-	_alpm_db_rewind(db);
 	while((info = _alpm_db_scan(db, NULL)) != NULL) {
 		_alpm_log(PM_LOG_FUNCTION, "adding '%s' to package cache for db '%s'\n",
 							alpm_pkg_get_name(info), db->treename);

lib/libalpm/db.h
@@ -60,16 +60,12 @@ int _alpm_prov_cmp(const void *provision, const void *needle);
 alpm_list_t *_alpm_db_whatprovides(pmdb_t *db, const char *package);
 
 /* be.c, backend specific calls */
-int _alpm_db_install(pmdb_t *db, const char *dbfile);
 int _alpm_db_open(pmdb_t *db);
 void _alpm_db_close(pmdb_t *db);
-void _alpm_db_rewind(pmdb_t *db);
 pmpkg_t *_alpm_db_scan(pmdb_t *db, const char *target);
 int _alpm_db_read(pmdb_t *db, pmpkg_t *info, pmdbinfrq_t inforeq);
 int _alpm_db_write(pmdb_t *db, pmpkg_t *info, pmdbinfrq_t inforeq);
 int _alpm_db_remove(pmdb_t *db, pmpkg_t *info);
-time_t _alpm_db_getlastupdate(const pmdb_t *db);
-int _alpm_db_setlastupdate(const pmdb_t *db, time_t time);
 
 #endif /* _ALPM_DB_H */
 

lib/libalpm/db.c
@@ -39,7 +39,6 @@
 #include "alpm_list.h"
 #include "log.h"
 #include "util.h"
-#include "dload.h"
 #include "handle.h"
 #include "cache.h"
 #include "alpm.h"
@@ -195,95 +194,6 @@ int SYMEXPORT alpm_db_setserver(pmdb_t *db, const char *url)
 	} else {
 		FREELIST(db->servers);
 		_alpm_log(PM_LOG_DEBUG, "serverlist flushed for '%s'\n", db->treename);
-	}
-
-	return(0);
-}
-
-/** Update a package database
- * @param force if true, then forces the update, otherwise update only in case
- * the database isn't up to date
- * @param db pointer to the package database to update
- * @return 0 on success, > 0 on error (pm_errno is set accordingly), < 0 if up
- * to date
- */
-int SYMEXPORT alpm_db_update(int force, pmdb_t *db)
-{
-	alpm_list_t *lp;
-	char path[PATH_MAX];
-	time_t newmtime = 0, lastupdate = 0;
-	const char *dbpath;
-	int ret;
-
-	ALPM_LOG_FUNC;
-
-	/* Sanity checks */
-	ASSERT(handle != NULL, RET_ERR(PM_ERR_HANDLE_NULL, -1));
-	ASSERT(db != NULL && db != handle->db_local, RET_ERR(PM_ERR_WRONG_ARGS, -1));
-	/* Verify we are in a transaction.  This is done _mainly_ because we need a DB
-	 * lock - if we update without a db lock, we may kludge some other pacman
-	 * process that _has_ a lock.
-	 */
-	ASSERT(handle->trans != NULL, RET_ERR(PM_ERR_TRANS_NULL, -1));
-	ASSERT(handle->trans->state == STATE_INITIALIZED, RET_ERR(PM_ERR_TRANS_NOT_INITIALIZED, -1));
-	ASSERT(handle->trans->type == PM_TRANS_TYPE_SYNC, RET_ERR(PM_ERR_TRANS_TYPE, -1));
-
-	if(!alpm_list_find_ptr(handle->dbs_sync, db)) {
-		RET_ERR(PM_ERR_DB_NOT_FOUND, -1);
-	}
-
-	if(!force) {
-		/* get the lastupdate time */
-		lastupdate = _alpm_db_getlastupdate(db);
-		if(lastupdate == 0) {
-			_alpm_log(PM_LOG_DEBUG, "failed to get lastupdate time for %s\n",
-					db->treename);
-		}
-	}
-
-	/* build a one-element list */
-	snprintf(path, PATH_MAX, "%s" DBEXT, db->treename);
-	dbpath = alpm_option_get_dbpath();
-
-	ret = _alpm_download_single_file(path, db->servers, dbpath,
-			lastupdate, &newmtime);
-
-	if(ret == 1) {
-		/* mtimes match, do nothing */
-		pm_errno = 0;
-		return(1);
-	} else if(ret == -1) {
-		/* pm_errno was set by the download code */
-		_alpm_log(PM_LOG_DEBUG, "failed to sync db: %s\n", alpm_strerrorlast());
-		return(-1);
-	} else {
-		/* form the path to the db location */
-		snprintf(path, PATH_MAX, "%s%s" DBEXT, dbpath, db->treename);
-
-		/* remove the old dir */
-		_alpm_log(PM_LOG_DEBUG, "flushing database %s\n", db->path);
-		for(lp = _alpm_db_get_pkgcache(db); lp; lp = lp->next) {
-			pmpkg_t *pkg = lp->data;
-			if(pkg && _alpm_db_remove(db, pkg) == -1) {
-				_alpm_log(PM_LOG_ERROR, _("could not remove database entry %s%s\n"), db->treename,
-									alpm_pkg_get_name(pkg));
-				RET_ERR(PM_ERR_DB_REMOVE, -1);
-			}
-		}
-
-		/* Cache needs to be rebuild */
-		_alpm_db_free_pkgcache(db);
-
-		/* uncompress the sync database */
-		if(_alpm_db_install(db, path) == -1) {
-			return -1;
-		}
-		/* if we have a new mtime, set the DB last update value */
-		if(newmtime) {
-			_alpm_log(PM_LOG_DEBUG, "sync: new mtime for %s: %ju\n",
-					db->treename, (uintmax_t)newmtime);
-			_alpm_db_setlastupdate(db, newmtime);
-		}
 	}
 
 	return(0);

commit f671147282e0f5c6a2e05c8cb7a0d5b72ef8cb61

Fix rewinddir regression by cleaning up db_scan
Commit 046003844739416ff6d168dd2dec76490adb0727 caused a regression when
rereading the pkgcache after updating the on-disk databases. A rewinddir
call was errantly removed.

Instead of replacing the call to rewindir, clean up this whole mess.
db_scan is used only once and with target == NULL so there was actually half
the code of db_scan which was unused. This is gone now and replaced by a
single new db_populate function.

Dan: add_sorted ended up being 3x slower than one msort at the end, so I
changed back to that. I also made one pointer variable const and merged this
whole patch with my original fix for the rewinddir issue.

Signed-off-by: Xavier Chantry <shiningxc@gmail.com>
Signed-off-by: Dan McGee <dan@archlinux.org>

lib/libalpm/be_files.c
@@ -177,8 +177,8 @@ int SYMEXPORT alpm_db_update(int force, pmdb_t *db)
 		for(lp = _alpm_db_get_pkgcache(db); lp; lp = lp->next) {
 			pmpkg_t *pkg = lp->data;
 			if(pkg && _alpm_db_remove(db, pkg) == -1) {
-				_alpm_log(PM_LOG_ERROR, _("could not remove database entry %s%s\n"), db->treename,
-									alpm_pkg_get_name(pkg));
+				_alpm_log(PM_LOG_ERROR, _("could not remove database entry %s%s\n"),
+						db->treename, pkg->name);
 				RET_ERR(PM_ERR_DB_REMOVE, -1);
 			}
 		}
@@ -273,104 +273,60 @@ static int splitname(const char *target, pmpkg_t *pkg)
 	return(0);
 }
 
-pmpkg_t *_alpm_db_scan(pmdb_t *db, const char *target)
+int _alpm_db_populate(pmdb_t *db)
 {
+	int count = 0;
 	struct dirent *ent = NULL;
 	struct stat sbuf;
 	char path[PATH_MAX];
-	char *ptr = NULL;
-	int found = 0;
-	pmpkg_t *pkg = NULL;
 
 	ALPM_LOG_FUNC;
 
-	if(db == NULL) {
-		RET_ERR(PM_ERR_DB_NULL, NULL);
-	}
+	ASSERT(db != NULL, RET_ERR(PM_ERR_DB_NULL, -1));
 
-	/* We loop here until we read a valid package.  When an iteration of this loop
-	 * fails, it means alpm_db_read failed to read a valid package, so we'll read
-	 * the next so as not to abort whole-db operations early
-	 */
-	while(!pkg) {
-		if(target != NULL) {
-			/* search for a specific package (by name only) */
-			rewinddir(db->handle);
-			while(!found && (ent = readdir(db->handle)) != NULL) {
-				char *name;
+	rewinddir(db->handle);
+	while((ent = readdir(db->handle)) != NULL) {
+		const char *name = ent->d_name;
+		pmpkg_t *pkg;
 
-				if(strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0) {
-					continue;
-				}
-				/* stat the entry, make sure it's a directory */
-				snprintf(path, PATH_MAX, "%s/%s", db->path, ent->d_name);
-				if(stat(path, &sbuf) || !S_ISDIR(sbuf.st_mode)) {
-					continue;
-				}
-
-				STRDUP(name, ent->d_name, return(NULL));
-
-				/* truncate the string at the second-to-last hyphen, */
-				/* which will give us the package name */
-				if((ptr = rindex(name, '-'))) {
-					*ptr = '\0';
-				}
-				if((ptr = rindex(name, '-'))) {
-					*ptr = '\0';
-				}
-				if(strcmp(name, target) == 0) {
-					found = 1;
-				}
-				FREE(name);
-			}
-
-			if(!found) {
-				return(NULL);
-			}
-		} else { /* target == NULL, full scan */
-			int isdir = 0;
-			while(!isdir) {
-				ent = readdir(db->handle);
-				if(ent == NULL) {
-					return(NULL);
-				}
-				if(strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0) {
-					isdir = 0;
-					continue;
-				}
-				/* stat the entry, make sure it's a directory */
-				snprintf(path, PATH_MAX, "%s/%s", db->path, ent->d_name);
-				if(!stat(path, &sbuf) && S_ISDIR(sbuf.st_mode)) {
-					isdir = 1;
-				}
-			}
+		if(strcmp(name, ".") == 0 || strcmp(name, "..") == 0) {
+			continue;
+		}
+		/* stat the entry, make sure it's a directory */
+		snprintf(path, PATH_MAX, "%s/%s", db->path, name);
+		if(stat(path, &sbuf) != 0 || !S_ISDIR(sbuf.st_mode)) {
+			continue;
 		}
 
 		pkg = _alpm_pkg_new(NULL, NULL);
 		if(pkg == NULL) {
-			_alpm_log(PM_LOG_DEBUG, "db scan could not find package: %s\n", target);
-			return(NULL);
+			return(-1);
 		}
 		/* split the db entry name */
-		if(splitname(ent->d_name, pkg) != 0) {
+		if(splitname(name, pkg) != 0) {
 			_alpm_log(PM_LOG_ERROR, _("invalid name for database entry '%s'\n"),
-					ent->d_name);
-			alpm_pkg_free(pkg);
-			pkg = NULL;
+					name);
+			_alpm_pkg_free(pkg);
 			continue;
 		}
 
 		/* explicitly read with only 'BASE' data, accessors will handle the rest */
 		if(_alpm_db_read(db, pkg, INFRQ_BASE) == -1) {
-			/* TODO removed corrupt entry from the FS here */
+			_alpm_log(PM_LOG_ERROR, _("corrupted database entry '%s'\n"), name);
 			_alpm_pkg_free(pkg);
-		} else {
-			pkg->origin = PKG_FROM_CACHE;
-			pkg->origin_data.db = db;
+			continue;
 		}
+		pkg->origin = PKG_FROM_CACHE;
+		pkg->origin_data.db = db;
+		/* add to the collection */
+		_alpm_log(PM_LOG_FUNCTION, "adding '%s' to package cache for db '%s'\n",
+				pkg->name, db->treename);
+		db->pkgcache = alpm_list_add(db->pkgcache, pkg);
+		count++;
 	}
 
-	return(pkg);
+	db->pkgcache = alpm_list_msort(db->pkgcache, count, _alpm_pkg_cmp);
+	return(count);
 }
 
 int _alpm_db_read(pmdb_t *db, pmpkg_t *info, pmdbinfrq_t inforeq)

lib/libalpm/cache.c
@@ -40,31 +40,21 @@
  */
 int _alpm_db_load_pkgcache(pmdb_t *db)
 {
-	pmpkg_t *info;
-	int count = 0;
-
 	ALPM_LOG_FUNC;
 
 	if(db == NULL) {
 		return(-1);
 	}
-
 	_alpm_db_free_pkgcache(db);
 
 	_alpm_log(PM_LOG_DEBUG, "loading package cache for repository '%s'\n",
-	          db->treename);
-
-	while((info = _alpm_db_scan(db, NULL)) != NULL) {
-		_alpm_log(PM_LOG_FUNCTION, "adding '%s' to package cache for db '%s'\n",
-							alpm_pkg_get_name(info), db->treename);
-		info->origin = PKG_FROM_CACHE;
-		info->origin_data.db = db;
-		/* add to the collection */
-		db->pkgcache = alpm_list_add(db->pkgcache, info);
-		count++;
+			db->treename);
+	if(_alpm_db_populate(db) == -1) {
+		_alpm_log(PM_LOG_DEBUG,
+				"failed to load package cache for repository '%s'\n", db->treename);
+		return(-1);
 	}
 
-	db->pkgcache = alpm_list_msort(db->pkgcache, count, _alpm_pkg_cmp);
 	return(0);
 }
 

lib/libalpm/db.h
@@ -58,7 +58,7 @@ pmdb_t *_alpm_db_register_sync(const char *treename);
 /* be.c, backend specific calls */
 int _alpm_db_open(pmdb_t *db);
 void _alpm_db_close(pmdb_t *db);
-pmpkg_t *_alpm_db_scan(pmdb_t *db, const char *target);
+int _alpm_db_populate(pmdb_t *db);
 int _alpm_db_read(pmdb_t *db, pmpkg_t *info, pmdbinfrq_t inforeq);
 int _alpm_db_write(pmdb_t *db, pmpkg_t *info, pmdbinfrq_t inforeq);
 int _alpm_db_remove(pmdb_t *db, pmpkg_t *info);