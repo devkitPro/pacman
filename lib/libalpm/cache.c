devkitPro/pacman

commit 8240da6cb3ff95ad480efe3e1876104024398fae

libalpm/cache.c : don't duplicate packages in pkgcache.
Edit _alpm_db_add_pkgincache to not duplicate packages, because this is not
needed, is slower, and uses more memory. This made the max memory usage
during base reinstall go from 10.4MB to 9.7MB.

Signed-off-by: Chantry Xavier <shiningxc@gmail.com>
Signed-off-by: Dan McGee <dan@archlinux.org>

lib/libalpm/cache.c
@@ -81,10 +81,7 @@ void _alpm_db_free_pkgcache(pmdb_t *db)
 	_alpm_log(PM_LOG_DEBUG, "freeing package cache for repository '%s'\n",
 	                        db->treename);
 
-	alpm_list_t *tmp;
-	for(tmp = db->pkgcache; tmp; tmp = alpm_list_next(tmp)) {
-		_alpm_pkg_free(tmp->data);
-	}
+	alpm_list_free_inner(db->pkgcache, (alpm_list_fn_free)_alpm_pkg_free);
 	alpm_list_free(db->pkgcache);
 	db->pkgcache = NULL;
 
@@ -115,21 +112,15 @@ alpm_list_t *_alpm_db_get_pkgcache(pmdb_t *db)
 
 int _alpm_db_add_pkgincache(pmdb_t *db, pmpkg_t *pkg)
 {
-	pmpkg_t *newpkg;
-
 	ALPM_LOG_FUNC;
 
 	if(db == NULL || pkg == NULL) {
 		return(-1);
 	}
 
-	newpkg = _alpm_pkg_dup(pkg);
-	if(newpkg == NULL) {
-		return(-1);
-	}
 	_alpm_log(PM_LOG_DEBUG, "adding entry '%s' in '%s' cache\n",
-						alpm_pkg_get_name(newpkg), db->treename);
-	db->pkgcache = alpm_list_add_sorted(db->pkgcache, newpkg, _alpm_pkg_cmp);
+						alpm_pkg_get_name(pkg), db->treename);
+	db->pkgcache = alpm_list_add_sorted(db->pkgcache, pkg, _alpm_pkg_cmp);
 
 	_alpm_db_free_grpcache(db);
 

lib/libalpm/trans.c
@@ -253,7 +253,8 @@ void _alpm_trans_free(pmtrans_t *trans)
 	FREELIST(trans->targets);
 	if(trans->type == PM_TRANS_TYPE_SYNC) {
 		alpm_list_free_inner(trans->packages, (alpm_list_fn_free)_alpm_sync_free);
-	} else {
+	} else if (trans->type == PM_TRANS_TYPE_REMOVE ||
+			trans->type == PM_TRANS_TYPE_REMOVEUPGRADE) {
 		alpm_list_free_inner(trans->packages, (alpm_list_fn_free)_alpm_pkg_free);
 	}
 	alpm_list_free(trans->packages);

commit 03021713e5315312b150e6ac9c5cf48fe68f7615

_alpm_db_add_pkgincache rework
Commit 8240da6cb3ff95ad480efe3e1876104024398fae broke some alpm hierarchy
and introduced a new memleak (trans->packages was never freed in case of add
transaction, even if the transaction wasn't committed), so it is reverted
now.

We follow a different approach to reduce memory usage:
_alpm_db_add_pkgincache doesn't duplicate the whole package before adding
it to the cache, only the package name and version (INFRQ_BASE).
This method needs very small extra memory (compared to the reverted method),
and after transaction commit we use less memory than before (since the
big 'files' fields are not copied to cache), this is useful in GUIs.

Note: The old add_pkgincache was a bit broken, since pkg->origin wasn't
filled in correctly.

Signed-off-by: Nagy Gabor <ngaba@bibl.u-szeged.hu>
Acked-by: Xavier Chantry <shiningxc@gmail.com>
Signed-off-by: Dan McGee <dan@archlinux.org>

lib/libalpm/cache.c
@@ -98,17 +98,35 @@ alpm_list_t *_alpm_db_get_pkgcache(pmdb_t *db)
 	return(db->pkgcache);
 }
 
+/* "duplicate" pkg with BASE info (to spare some memory) then add it to pkgcache */
 int _alpm_db_add_pkgincache(pmdb_t *db, pmpkg_t *pkg)
 {
+	pmpkg_t *newpkg;
+
 	ALPM_LOG_FUNC;
 
 	if(db == NULL || pkg == NULL) {
 		return(-1);
 	}
 
+	newpkg = _alpm_pkg_new();
+	if(newpkg == NULL) {
+		return(-1);
+	}
+	newpkg->name = strdup(pkg->name);
+	newpkg->version = strdup(pkg->version);
+	if(newpkg->name == NULL || newpkg->version == NULL) {
+		pm_errno = PM_ERR_MEMORY;
+		_alpm_pkg_free(newpkg);
+		return(-1);
+	}
+	newpkg->origin = PKG_FROM_CACHE;
+	newpkg->origin_data.db = db;
+	newpkg->infolevel = INFRQ_BASE; 
+
 	_alpm_log(PM_LOG_DEBUG, "adding entry '%s' in '%s' cache\n",
-						alpm_pkg_get_name(pkg), db->treename);
-	db->pkgcache = alpm_list_add_sorted(db->pkgcache, pkg, _alpm_pkg_cmp);
+						alpm_pkg_get_name(newpkg), db->treename);
+	db->pkgcache = alpm_list_add_sorted(db->pkgcache, newpkg, _alpm_pkg_cmp);
 
 	_alpm_db_free_grpcache(db);
 

lib/libalpm/trans.c
@@ -251,8 +251,7 @@ void _alpm_trans_free(pmtrans_t *trans)
 
 	if(trans->type == PM_TRANS_TYPE_SYNC) {
 		alpm_list_free_inner(trans->packages, (alpm_list_fn_free)_alpm_sync_free);
-	} else if (trans->type == PM_TRANS_TYPE_REMOVE ||
-			trans->type == PM_TRANS_TYPE_REMOVEUPGRADE) {
+	} else {
 		alpm_list_free_inner(trans->packages, (alpm_list_fn_free)_alpm_pkg_free);
 	}
 	alpm_list_free(trans->packages);