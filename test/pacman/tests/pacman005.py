46ffd342a4255916a9599692b5969ae4cd85a156
Jakob Gruber <jakob.gruber@gmail.com>
2010-10-09 09:20:57 +0200

    CLI args: add pactest with an invalid combination
    Pacman should catch cases in which the passed arguments don't apply to the
    current operation (sync/query/...).
    
    Also see FS#20950.
    
    Signed-off-by: Dan McGee <dan@archlinux.org>

---
+++ test/pacman/tests/pacman005.py
@@ -0,0 +1,11 @@
+self.description = "Test invalid combination of command line options (-Qy)"
+
+p = pmpkg("foobar")
+self.addpkg2db("local", p)
+
+self.args = "-Qy"
+
+self.addrule("PACMAN_RETCODE=1")
+
+self.expectfailure = True
+
