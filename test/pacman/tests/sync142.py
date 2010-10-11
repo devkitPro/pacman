73886504d7fa62e8cb49631fb13b53ea69919a3f
Dan McGee <dan@archlinux.org>
2010-10-11 17:39:41 -0500

    Add several pactests for epoch code
    Signed-off-by: Dan McGee <dan@archlinux.org>

---
+++ test/pacman/tests/sync142.py
@@ -0,0 +1,14 @@
+self.description = "Sysupgrade with an epoch package overriding a force package"
+
+sp = pmpkg("dummy", "1.4-1")
+sp.epoch = 2
+self.addpkg2db("sync", sp)
+
+lp = pmpkg("dummy", "2.0-1")
+lp.force = 1
+self.addpkg2db("local", lp)
+
+self.args = "-Su"
+
+self.addrule("PACMAN_RETCODE=0")
+self.addrule("PKG_VERSION=dummy|1.4-1")
