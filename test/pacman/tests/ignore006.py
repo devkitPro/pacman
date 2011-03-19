58df372be6758cfe00069ac7279cd92e3b61edef
Pang Yan Han <pangyanhan@gmail.com>
2011-03-19 16:09:27 +0800

    Fix handling of ignored packages
    Noted in FS#23342. When the user attempts to install an ignored package
    and answers no when asked whether to install it, pacman bails out with:
    
    "error: target not found: packagename"
    
    This is because satisfiers are not found for the package and execution
    continues to process_group(), where the package is treated as a group
    (which does not exist).
    
    In addition, test ignore006.py is updated with PACMAN_RETCODE=0 since
    saying no to installing an ignored package should not be considered an
    error.
    
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>
    Signed-off-by: Xavier Chantry <chantry.xavier@gmail.com>
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- test/pacman/tests/ignore006.py
+++ test/pacman/tests/ignore006.py
@@ -6,5 +6,5 @@ self.addpkg2db("sync", pkg)
 self.option["IgnorePkg"] = ["package1"]
 self.args = "--ask=1 -S %s" % pkg.name
 
-self.addrule("PACMAN_RETCODE=1")
+self.addrule("PACMAN_RETCODE=0")
 self.addrule("!PKG_EXIST=package1")
