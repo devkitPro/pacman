f6716ae94a5f299569b35bebb4d0aae23cd9af97
Allan McRae <allan@archlinux.org>
2011-03-19 11:22:19 +1000

    Use sane umask for repo db downloads
    Fixes FS#23343.
    
    Signed-off-by: Allan McRae <allan@archlinux.org>
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- lib/libalpm/be_sync.c
+++ lib/libalpm/be_sync.c
@@ -84,6 +84,7 @@ int SYMEXPORT alpm_db_update(int force, pmdb_t *db)
 	struct stat buf;
 	size_t len;
 	int ret;
+	mode_t oldmask;
 
 	ALPM_LOG_FUNC;
 
@@ -103,6 +104,9 @@ int SYMEXPORT alpm_db_update(int force, pmdb_t *db)
 	len = strlen(dbpath) + 6;
 	MALLOC(syncpath, len, RET_ERR(PM_ERR_MEMORY, -1));
 	sprintf(syncpath, "%s%s", dbpath, "sync/");
+
+	/* make sure we have a sane umask */
+	oldmask = umask(0022);
 
 	if(stat(syncpath, &buf) != 0) {
 		_alpm_log(PM_LOG_DEBUG, "database dir '%s' does not exist, creating it\n",
@@ -124,6 +128,7 @@ int SYMEXPORT alpm_db_update(int force, pmdb_t *db)
 	ret = _alpm_download_single_file(dbfile, db->servers, syncpath, force);
 	free(dbfile);
 	free(syncpath);
+	umask(oldmask);
 
 	if(ret == 1) {
 		/* files match, do nothing */
