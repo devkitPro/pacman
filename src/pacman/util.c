devkitPro/pacman

commit c1f742d7750a14b680d2ceb2c75d952ccb53585d

Ensure list_display works on outputs of unknown width
If getcols() returns 0, we were getting stuck before in a loop of no
return. Teach getcols() to take a default value to return if the width
is unknown, and use this everywhere as appropriate.

Also make a few other cleanups while diagnosing this issue, such as
const-ifying some variables.

Noticed-by: Dave Reisner <d@falconindy.com>
Signed-off-by: Dan McGee <dan@archlinux.org>

src/pacman/util.c
@@ -101,7 +101,7 @@ int needs_root(void)
 }
 
 /* gets the current screen column width */
-int getcols(void)
+int getcols(int def)
 {
 #ifdef TIOCGSIZE
 	struct ttysize win;
@@ -114,7 +114,7 @@ int getcols(void)
 		return win.ws_col;
 	}
 #endif
-	return 0;
+	return def;
 }
 
 /* does the same thing as 'rm -rf' */
@@ -209,13 +209,12 @@ void indentprint(const char *str, int indent)
 {
 	wchar_t *wcstr;
 	const wchar_t *p;
-	int len, cidx, cols;
+	int len, cidx;
+	const int cols = getcols(0);
 
 	if(!str) {
 		return;
 	}
-
-	cols = getcols();
 
 	/* if we're not a tty, print without indenting */
 	if(cols == 0) {
@@ -425,8 +424,6 @@ static int string_length(const char *s)
 
 void string_display(const char *title, const char *string)
 {
-	int len = 0;
-
 	if(title) {
 		printf("%s ", title);
 	}
@@ -434,7 +431,7 @@ void string_display(const char *title, const char *string)
 		printf(_("None"));
 	} else {
 		/* compute the length of title + a space */
-		len = string_length(title) + 1;
+		int len = string_length(title) + 1;
 		indentprint(string, len);
 	}
 	printf("\n");
@@ -443,7 +440,7 @@ void string_display(const char *title, const char *string)
 void list_display(const char *title, const alpm_list_t *list)
 {
 	const alpm_list_t *i;
-	int cols, len = 0;
+	int len = 0;
 
 	if(title) {
 		len = string_length(title) + 1;
@@ -453,11 +450,12 @@ void list_display(const char *title, const alpm_list_t *list)
 	if(!list) {
 		printf("%s\n", _("None"));
 	} else {
+		int cols;
+		const int maxcols = getcols(80);
 		for(i = list, cols = len; i; i = alpm_list_next(i)) {
 			char *str = alpm_list_getdata(i);
 			int s = string_length(str);
-			int maxcols = getcols();
-			if(maxcols > 0 && (cols + s + 2) >= maxcols) {
+			if(cols + s + 2 >= maxcols) {
 				int j;
 				cols = len;
 				printf("\n");

src/pacman/callback.c
@@ -351,7 +351,9 @@ void cb_trans_progress(pmtransprog_t event, const char *pkgname, int percent,
 	int len, wclen, wcwid, padwid;
 	wchar_t *wcstr;
 
-	if(config->noprogressbar) {
+	const int cols = getcols(0);
+
+	if(config->noprogressbar || cols == 0) {
 		return;
 	}
 
@@ -397,7 +399,7 @@ void cb_trans_progress(pmtransprog_t event, const char *pkgname, int percent,
 			return;
 	}
 
-	infolen = getcols() * 6 / 10;
+	infolen = cols * 6 / 10;
 	if (infolen < 50) {
 		infolen = 50;
 	}
@@ -454,7 +456,7 @@ void cb_trans_progress(pmtransprog_t event, const char *pkgname, int percent,
 	free(wcstr);
 
 	/* call refactored fill progress function */
-	fill_progress(percent, percent, getcols() - infolen);
+	fill_progress(percent, percent, cols - infolen);
 
 	if(percent == 100) {
 		alpm_list_t *i = NULL;
@@ -497,7 +499,9 @@ void cb_dl_progress(const char *filename, off_t file_xfered, off_t file_total)
 	int file_percent = 0, total_percent = 0;
 	char rate_size = 'K', xfered_size = 'K';
 
-	if(config->noprogressbar || file_total == -1) {
+	const int cols = getcols(0);
+
+	if(config->noprogressbar || cols == 0 || file_total == -1) {
 		if(file_xfered == 0) {
 			printf(_("downloading %s...\n"), filename);
 			fflush(stdout);
@@ -505,7 +509,7 @@ void cb_dl_progress(const char *filename, off_t file_xfered, off_t file_total)
 		return;
 	}
 
-	infolen = getcols() * 6 / 10;
+	infolen = cols * 6 / 10;
 	if (infolen < 50) {
 		infolen = 50;
 	}
@@ -662,9 +666,9 @@ void cb_dl_progress(const char *filename, off_t file_xfered, off_t file_total)
 	free(wcfname);
 
 	if(totaldownload) {
-		fill_progress(file_percent, total_percent, getcols() - infolen);
+		fill_progress(file_percent, total_percent, cols - infolen);
 	} else {
-		fill_progress(file_percent, file_percent, getcols() - infolen);
+		fill_progress(file_percent, file_percent, cols - infolen);
 	}
 	return;
 }

src/pacman/util.h
@@ -42,7 +42,7 @@
 int trans_init(pmtransflag_t flags);
 int trans_release(void);
 int needs_root(void);
-int getcols(void);
+int getcols(int def);
 int rmrf(const char *path);
 const char *mbasename(const char *path);
 char *mdirname(const char *path);

commit 895a88886525dd3e6d2d0db996cedb7e73455fbb

Fix list_display on non-ttys and other output fixes
commit c1f742d7750a14 broke what was one of the tenants of out output-
if piping pacman output somewhere else, we shouldn't ever try to
line-wrap and indent print our output. This makes it easier for tools to
use output from pacman -Ss, -Qs, -Qi, etc. list_display() unfortunately
was given a default value of 80 rather than 0, so fix this.

Next, make some additional changes that ensure we don't insert an
unnecessary blank line if for some crazy reason the indent level (such
as on -Qi output) is greater than the number of columns. Accomplish this
by printing the first item unconditionally as we do in
list_display_linebreak().

Finally, teach indentprint to not wrap if the number of columns is less
than the indent level, this prevents some forms of ridiculous output
such as the following:

    Install Date   : Wed
                     08
                     Jun
                     2011
                     04:39:19
                     AM
                     CDT

Signed-off-by: Dan McGee <dan@archlinux.org>

src/pacman/util.c
@@ -216,8 +216,9 @@ void indentprint(const char *str, int indent)
 		return;
 	}
 
-	/* if we're not a tty, print without indenting */
-	if(cols == 0) {
+	/* if we're not a tty, or our tty is not wide enough that wrapping even makes
+	 * sense, print without indenting */
+	if(cols == 0 || indent > cols) {
 		printf("%s", str);
 		return;
 	}
@@ -450,12 +451,16 @@ void list_display(const char *title, const alpm_list_t *list)
 	if(!list) {
 		printf("%s\n", _("None"));
 	} else {
-		int cols;
-		const int maxcols = getcols(80);
-		for(i = list, cols = len; i; i = alpm_list_next(i)) {
-			char *str = alpm_list_getdata(i);
+		const int maxcols = getcols(0);
+		int cols = len;
+		const char *str = alpm_list_getdata(list);
+		printf("%s", str);
+		cols += string_length(str);
+		for(i = alpm_list_next(list), cols = len; i; i = alpm_list_next(i)) {
+			const char *str = alpm_list_getdata(i);
 			int s = string_length(str);
-			if(cols + s + 2 >= maxcols) {
+			/* wrap only if we have enough usable column space */
+			if(maxcols > len && cols + s + 2 >= maxcols) {
 				int j;
 				cols = len;
 				printf("\n");