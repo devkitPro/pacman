7480df68ce7fe60e93dd833faf83dd5f2d6149c1
Dan McGee <dan@archlinux.org>
2011-08-29 07:48:59 -0500

    Rework scriptlet tests to not depend on pacman output
    This actually does something in a scriptlet we can check with our normal
    set of rules, rather than relying on pacman debug output.
    
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- test/pacman/tests/scriptlet001.py
+++ test/pacman/tests/scriptlet001.py
@@ -2,18 +2,15 @@ self.description = "Scriptlet test (pre/post install)"
 
 p1 = pmpkg("dummy")
 p1.files = ['etc/dummy.conf']
-pre = "OUTPUT FROM PRE_INSTALL"
-post = "OUTPUT FROM POST_INSTALL"
-p1.install['pre_install'] = "echo " + pre
-p1.install['post_install'] = "echo " + post
+p1.install['pre_install'] = "echo foobar > pre_install"
+p1.install['post_install'] = "echo foobar > post_install"
 self.addpkg(p1)
 
-# --debug is necessary to check PACMAN_OUTPUT
-self.args = "--debug -U %s" % p1.filename()
+self.args = "-U %s" % p1.filename()
 
 self.addrule("PACMAN_RETCODE=0")
-self.addrule("PACMAN_OUTPUT=" + pre)
-self.addrule("PACMAN_OUTPUT=" + post)
+self.addrule("FILE_EXIST=pre_install")
+self.addrule("FILE_EXIST=post_install")
 
 fakechroot = util.which("fakechroot")
 if not fakechroot:
--- test/pacman/tests/scriptlet002.py
+++ test/pacman/tests/scriptlet002.py
@@ -2,18 +2,15 @@ self.description = "Scriptlet test (pre/post remove)"
 
 p1 = pmpkg("dummy")
 p1.files = ['etc/dummy.conf']
-pre = "OUTPUT FROM PRE_REMOVE";
-post = "OUTPUT FROM POST_REMOVE";
-p1.install['pre_remove'] = "echo " + pre
-p1.install['post_remove'] = "echo " + post
+p1.install['pre_remove'] = "echo foobar > pre_remove"
+p1.install['post_remove'] = "echo foobar > post_remove"
 self.addpkg2db("local", p1)
 
-# --debug is necessary to check PACMAN_OUTPUT
-self.args = "--debug -R %s" % p1.name
+self.args = "-R %s" % p1.name
 
 self.addrule("PACMAN_RETCODE=0")
-self.addrule("PACMAN_OUTPUT=" + pre)
-self.addrule("PACMAN_OUTPUT=" + post)
+self.addrule("FILE_EXIST=pre_remove")
+self.addrule("FILE_EXIST=post_remove")
 
 fakechroot = util.which("fakechroot")
 if not fakechroot:
