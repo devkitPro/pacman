e253968b4df079faf91266639be1b8b2cff6e2b5
Allan McRae <allan@archlinux.org>
2012-06-07 16:21:05 +1000

    repo-add: add checkdepends information
    Similar to the case for makedepends, it is useful to be able to
    access this information without parsing a PKGBUILD.
    
    Signed-off-by: Allan McRae <allan@archlinux.org>

--- lib/libalpm/be_sync.c
+++ lib/libalpm/be_sync.c
@@ -597,6 +597,12 @@ static int sync_db_read(alpm_db_t *db, struct archive *archive,
 					READ_NEXT();
 					if(strlen(line) == 0) break;
 				}
+			} else if(strcmp(line, "%CHECKDEPENDS%") == 0) {
+				/* currently unused */
+				while(1) {
+					READ_NEXT();
+					if(strlen(line) == 0) break;
+				}
 			} else if(strcmp(line, "%CONFLICTS%") == 0) {
 				READ_AND_SPLITDEP(pkg->conflicts);
 			} else if(strcmp(line, "%PROVIDES%") == 0) {
