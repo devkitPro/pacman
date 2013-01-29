7edd262a060c508a3a08b73dc18158eeac16c053
Dave Reisner <dreisner@archlinux.org>
2013-01-29 09:25:18 -0500

    inline libarchive compat wrappers
    Suggested-by: Dan McGee <dan@archlinux.org>
    Signed-off-by: Dave Reisner <dreisner@archlinux.org>
    Signed-off-by: Allan McRae <allan@archlinux.org>

--- lib/libalpm/libarchive-compat.h
+++ lib/libalpm/libarchive-compat.h
@@ -1,3 +1,6 @@
+#ifndef _LIBARCHIVE_COMPAT_H
+#define _LIBARCHIVE_COMPAT_H
+
 /*
  * libarchive-compat.h
  *
@@ -16,18 +19,55 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef _LIBARCHIVE_H
-#define _LIBARCHIVE_H
 
-#include <archive.h>
+#include <stdint.h>
 
-int _alpm_archive_read_free(struct archive *archive);
-int64_t _alpm_archive_compressed_ftell(struct archive *archive);
-int _alpm_archive_read_open_file(struct archive *archive,
-		const char *filename, size_t block_size);
-int _alpm_archive_filter_code(struct archive *archive);
-int _alpm_archive_read_support_filter_all(struct archive *archive);
+static inline int _alpm_archive_read_free(struct archive *archive)
+{
+#if ARCHIVE_VERSION_NUMBER >= 3000000
+	return archive_read_free(archive);
+#else
+	return archive_read_finish(archive);
+#endif
+}
 
-#endif /* _LIBARCHIVE_H */
+static inline int64_t _alpm_archive_compressed_ftell(struct archive *archive)
+{
+#if ARCHIVE_VERSION_NUMBER >= 3000000
+	return archive_filter_bytes(archive, -1);
+#else
+	return archive_position_compressed(archive);
+#endif
+}
+
+static inline int _alpm_archive_read_open_file(struct archive *archive,
+		const char *filename, size_t block_size)
+{
+#if ARCHIVE_VERSION_NUMBER >= 3000000
+	return archive_read_open_filename(archive, filename, block_size);
+#else
+	return archive_read_open_file(archive, filename, block_size);
+#endif
+}
+
+static inline int _alpm_archive_filter_code(struct archive *archive)
+{
+#if ARCHIVE_VERSION_NUMBER >= 3000000
+	return archive_filter_code(archive, 0);
+#else
+	return archive_compression(archive);
+#endif
+}
+
+static inline int _alpm_archive_read_support_filter_all(struct archive *archive)
+{
+#if ARCHIVE_VERSION_NUMBER >= 3000000
+	return archive_read_support_filter_all(archive);
+#else
+	return archive_read_support_compression_all(archive);
+#endif
+}
+
+#endif /* _LIBARCHIVE_COMPAT_H */
 
 /* vim: set ts=2 sw=2 noet: */
