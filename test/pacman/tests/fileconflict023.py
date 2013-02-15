4a1d93b92c76bde58193083812dda94c0fa5d373
Andrew Gregory <andrew.gregory.8@gmail.com>
2013-02-15 18:13:51 -0500

    add fileconflict tests for cases with symlinks
    Signed-off-by: Andrew Gregory <andrew.gregory.8@gmail.com>
    Signed-off-by: Allan McRae <allan@archlinux.org>

---
+++ test/pacman/tests/fileconflict023.py
@@ -0,0 +1,20 @@
+self.description = "File conflict between package with symlink and package with real path resolved by removal"
+
+self.filesystem = ["usr/", "usr/lib/", "lib -> usr/lib/"]
+
+lp1 = pmpkg("foo")
+lp1.files = ["lib/", "lib/file"]
+self.addpkg2db("local", lp1)
+
+sp1 = pmpkg("bar")
+sp1.conflicts = ["foo"]
+sp1.files = ["usr/", "usr/lib/", "usr/lib/file"]
+self.addpkg2db("sync", sp1)
+
+self.args = "-S %s --ask=4" % sp1.name
+
+self.addrule("PACMAN_RETCODE=0")
+self.addrule("!PKG_EXIST=foo")
+self.addrule("PKG_EXIST=bar")
+
+self.expectfailure = True
