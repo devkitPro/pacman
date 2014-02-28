0b0fe51a2cb1b5d8b5031b4299547a4e5fd47c20
Andrew Gregory <andrew.gregory.8@gmail.com>
2014-02-28 10:13:33 -0500

    pmpkg.py: remove unused imports
    Signed-off-by: Andrew Gregory <andrew.gregory.8@gmail.com>
    Signed-off-by: Allan McRae <allan@archlinux.org>

--- test/pacman/pmpkg.py
+++ test/pacman/pmpkg.py
@@ -14,11 +14,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 import os
-import tempfile
-import stat
-import shutil
 from StringIO import StringIO
 import tarfile
 
