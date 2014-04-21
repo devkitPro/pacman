devkitPro/pacman

commit 92216c5864efccacf2daa3f4f15de3bb479054bb

pacman/util: return size_t from strtrim
Instead of returning the same value as the parameter to this function,
return the length of the string, which can be useful to the caller when
its non-zero (e.g. to find the end of the string).

Signed-off-by: Dave Reisner <dreisner@archlinux.org>

src/pacman/util.c
@@ -325,13 +325,13 @@ char *strtoupper(char *str)
 
 /* Trim whitespace and newlines from a string
  */
-char *strtrim(char *str)
+size_t strtrim(char *str)
 {
-	char *pch = str;
+	char *end, *pch = str;
 
 	if(str == NULL || *str == '\0') {
 		/* string is empty, so we're done. */
-		return str;
+		return 0;
 	}
 
 	while(isspace((unsigned char)*pch)) {
@@ -348,16 +348,16 @@ char *strtrim(char *str)
 
 	/* check if there wasn't anything but whitespace in the string. */
 	if(*str == '\0') {
-		return str;
+		return 0;
 	}
 
-	pch = (str + (strlen(str) - 1));
-	while(isspace((unsigned char)*pch)) {
-		pch--;
+	end = (str + strlen(str) - 1);
+	while(isspace((unsigned char)*end)) {
+		end--;
 	}
-	*++pch = '\0';
+	*++end = '\0';
 
-	return str;
+	return end - pch;
 }
 
 /* Replace all occurances of 'needle' with 'replace' in 'str', returning

src/pacman/util.h
@@ -56,7 +56,7 @@ const char *mbasename(const char *path);
 char *mdirname(const char *path);
 void indentprint(const char *str, size_t indent);
 char *strtoupper(char *str);
-char *strtrim(char *str);
+size_t strtrim(char *str);
 char *strreplace(const char *str, const char *needle, const char *replace);
 alpm_list_t *strsplit(const char *str, const char splitchar);
 void string_display(const char *title, const char *string);

commit 5293d5784049869c965ee21f424b8845bdebb745

strtrim: reset pointer after trimming leading whitespace
Breakage introduced in 92216c5864efccac when we changed the signature
of strtrim to return something more useful.

Fixes FS#39976.

Signed-off-by: Allan McRae <allan@archlinux.org>

src/pacman/util.c
@@ -299,6 +299,7 @@ size_t strtrim(char *str)
 		size_t len = strlen(pch);
 		if(len) {
 			memmove(str, pch, len + 1);
+			pch = str;
 		} else {
 			*str = '\0';
 		}

src/util/pactree.c
@@ -159,6 +159,7 @@ static size_t strtrim(char *str)
 		size_t len = strlen(pch);
 		if(len) {
 			memmove(str, pch, len + 1);
+			pch = str;
 		} else {
 			*str = '\0';
 		}