d38c098787ebbedb0f55742dd547a20a7362b0cb
Allan McRae <allan@archlinux.org>
2015-05-12 23:08:22 +1000

    libmakepkg: extract in_array

---
+++ scripts/libmakepkg/util/util.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+#   util.sh - general utility functions
+#
+#   Copyright (c) 2006-2015 Pacman Development Team <pacman-dev@archlinux.org>
+#   Copyright (c) 2002-2006 by Judd Vinet <jvinet@zeroflux.org>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+[ -n "$LIBMAKEPKG_UTIL_UTIL_SH" ] && return
+LIBMAKEPKG_UTIL_UTIL_SH=1
+
+
+##
+#  usage : in_array( $needle, $haystack )
+# return : 0 - found
+#          1 - not found
+##
+in_array() {
+	local needle=$1; shift
+	local item
+	for item in "$@"; do
+		[[ $item = "$needle" ]] && return 0 # Found
+	done
+	return 1 # Not Found
+}
