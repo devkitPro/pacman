877578762cf47a9820bcc3cabbc138fe19ce3365
Allan McRae <allan@archlinux.org>
2015-07-20 14:50:52 +1000

    libmakepkg: move color settings to function
    Signed-off-by: Allan McRae <allan@archlinux.org>

--- scripts/libmakepkg/util/message.sh
+++ scripts/libmakepkg/util/message.sh
@@ -23,6 +23,26 @@
 LIBMAKEPKG_UTIL_MESSAGE_SH=1
 
 
+colorize() {
+	# prefer terminal safe colored and bold text when tput is supported
+	if tput setaf 0 &>/dev/null; then
+		ALL_OFF="$(tput sgr0)"
+		BOLD="$(tput bold)"
+		BLUE="${BOLD}$(tput setaf 4)"
+		GREEN="${BOLD}$(tput setaf 2)"
+		RED="${BOLD}$(tput setaf 1)"
+		YELLOW="${BOLD}$(tput setaf 3)"
+	else
+		ALL_OFF="\e[0m"
+		BOLD="\e[1m"
+		BLUE="${BOLD}\e[34m"
+		GREEN="${BOLD}\e[32m"
+		RED="${BOLD}\e[31m"
+		YELLOW="${BOLD}\e[33m"
+	fi
+	readonly ALL_OFF BOLD BLUE GREEN RED YELLOW
+}
+
 plain() {
 	local mesg=$1; shift
 	printf "${BOLD}    ${mesg}${ALL_OFF}\n" "$@" >&2
