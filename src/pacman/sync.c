ee960048b2394557f7155f053bf3ff5c8bb4b5e1
Allan McRae <allan@archlinux.org>
2016-03-29 20:21:42 +1000

    Remove notification of system upgrade when only printing URLs
    Signed-off-by: Allan McRae <allan@archlinux.org>

--- src/pacman/sync.c
+++ src/pacman/sync.c
@@ -694,9 +694,11 @@ static int sync_trans(alpm_list_t *targets)
 	}
 
 	if(config->op_s_upgrade) {
-		colon_printf(_("Starting full system upgrade...\n"));
-		alpm_logaction(config->handle, PACMAN_CALLER_PREFIX,
-				"starting full system upgrade\n");
+		if(!config->print) {
+			colon_printf(_("Starting full system upgrade...\n"));
+			alpm_logaction(config->handle, PACMAN_CALLER_PREFIX,
+					"starting full system upgrade\n");
+		}
 		if(alpm_sync_sysupgrade(config->handle, config->op_s_upgrade >= 2) == -1) {
 			pm_printf(ALPM_LOG_ERROR, "%s\n", alpm_strerror(alpm_errno(config->handle)));
 			trans_release();
