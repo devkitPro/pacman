930d50194754e9c75bc0d6d7ccc41ae8f4b4e5c4
Allan McRae <allan@archlinux.org>
2018-05-30 08:45:12 +1000

    pacman-conf: fix detection of repo usage
    pacman-conf returned All for any repo Usage query because it was
    checking if any repo options were enabled rather than if all options
    were enabled.
    
    Signed-off-by: Allan McRae <allan@archlinux.org>

--- src/pacman/pacman-conf.c
+++ src/pacman/pacman-conf.c
@@ -212,9 +212,9 @@ static void show_siglevel(const char *directive, alpm_siglevel_t level, int pkgo
 	}
 }
 
-static void show_usage(const char *directive, alpm_db_usage_t usage)
+static void show_usage(const char *directive, int usage)
 {
-	if(usage & ALPM_DB_USAGE_ALL) {
+	if(usage == ALPM_DB_USAGE_ALL) {
 		show_str(directive, "All");
 	} else {
 		if(usage & ALPM_DB_USAGE_SYNC) {
