0be1d4e5d0eb7f25d0c7cf8a296f759e1a121686
Allan McRae <allan@archlinux.org>
2022-01-02 18:06:15 +1000

    Remove parsing of long removed files in .PKGINFO
    These fields have not existed for many years.
    
    Signed-off-by: Allan McRae <allan@archlinux.org>

--- lib/libalpm/be_package.c
+++ lib/libalpm/be_package.c
@@ -244,10 +244,6 @@ static int parse_descfile(alpm_handle_t *handle, struct archive *a, alpm_pkg_t *
 				CALLOC(backup, 1, sizeof(alpm_backup_t), return -1);
 				STRDUP(backup->name, ptr, FREE(backup); return -1);
 				newpkg->backup = alpm_list_add(newpkg->backup, backup);
-			} else if(strcmp(key, "force") == 0) {
-				/* deprecated, skip it */
-			} else if(strcmp(key, "makepkgopt") == 0) {
-				/* not used atm */
 			} else {
 				_alpm_log(handle, ALPM_LOG_DEBUG, "%s: unknown key '%s' in description file line %d\n",
 									newpkg->name ? newpkg->name : "error", key, linenum);
